5c47cc0ba1820e587bc9a25e72189d687b8fe79f
Alok Singh <thealoksinghh@gmail.com>
2025-12-17 04:06:39 +0530

    icon added at common appbar and implemented at job detail screen

--- src/screens/Jobs/JobDetailedScreen.jsx
+++ src/screens/Jobs/JobDetailedScreen.jsx
@@ -9,6 +9,7 @@ import { getJobById } from '../../store/thunks/jobThunk';
 import Video from 'react-native-video';
 import { selectToken } from '../../store/selector';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { CommonAppBar } from '../../components/CommonComponents';
 
 // Mock data for Task Details section
 const jobDetailsData = [
@@ -229,16 +230,11 @@ export default function JobDetailedScreen({ navigation, route }) {
     <SafeAreaView style={styles.container}>
           <MyStatusBar/>
       {/* Header */}
-      <View style={styles.header}>
-        <TouchableOpacity style={styles.iconButton} onPress={() => navigation.goBack()}>
-          <Feather name="arrow-left" size={24} color="#111827" />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Task Details</Text>
-        <TouchableOpacity style={styles.iconButton}>
-          <Feather name="bell" size={24} color="#111827" />
-          <View style={styles.notificationDot} />
-        </TouchableOpacity>
-      </View>
+     <CommonAppBar
+      title="Task Details"
+      onBackPress={() => navigation.goBack()}
+      icon={"bell"}
+      method={() => {}} />
 
       <ScrollView contentContainerStyle={styles.scrollContent}>
         {/* Attachments Carousel */}
